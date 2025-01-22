e3f9817045e06ea689ac50c9533f191802407b86
fabP258 <fabio894@gmail.com>
2025-01-22 22:21:51 +0100

    Refactor residual stack

--- VQ_VAE/vq_vae.py
+++ VQ_VAE/vq_vae.py
@@ -1,7 +1,6 @@
 import torch
 import numpy as np
 from pathlib import Path
-from typing import Union
 
 from torch import nn
 from torch.nn import functional as F
@@ -9,39 +8,54 @@ from torch.nn import functional as F
 from scipy.cluster.vq import kmeans2
 
 
-class ResidualStack(nn.Module):
-    def __init__(
-        self, num_hiddens: int, num_residual_layers: int, num_residual_hiddens: int
-    ):
-        super().__init__()
-        layers = []
-        for i in range(num_residual_layers):
-            layers.append(
-                nn.Sequential(
-                    nn.ReLU(),
-                    nn.Conv2d(
-                        in_channels=num_hiddens,
-                        out_channels=num_residual_hiddens,
-                        kernel_size=3,
-                        padding=1,
-                    ),
-                    nn.ReLU(),
-                    nn.Conv2d(
-                        in_channels=num_residual_hiddens,
-                        out_channels=num_hiddens,
-                        kernel_size=1,
-                    ),
-                )
-            )
+class ResidualBlock(nn.Module):
 
-        self.layers = nn.ModuleList(layers)
+    def __init__(self, in_channels: int, num_hiddens: int, num_residual_hiddens: int):
+        super().__init__()
+        self._block = nn.Sequential(
+            nn.ReLU(True),
+            nn.Conv2d(
+                in_channels=in_channels,
+                out_channels=num_residual_hiddens,
+                kernel_size=3,
+                padding=1,
+                bias=False,
+            ),
+            nn.ReLU(True),
+            nn.Conv2d(
+                in_channels=num_residual_hiddens,
+                out_channels=num_hiddens,
+                kernel_size=1,
+                stride=1,
+                bias=False,
+            ),
+        )
 
     def forward(self, x):
-        h = x
-        for layer in self.layers:
-            h = h + layer(h)
+        return x + self._block(x)
 
-        return torch.relu(h)
+
+class ResidualStack(nn.Module):
+    def __init__(
+        self,
+        num_hiddens: int,
+        num_residual_layers: int,
+        num_residual_hiddens: int,
+    ):
+        super().__init__()
+        self._num_residual_layers = num_residual_layers
+        self._layers = nn.ModuleList(
+            [
+                ResidualBlock(num_hiddens, num_hiddens, num_residual_hiddens)
+                for _ in range(num_residual_layers)
+            ]
+        )
+
+    def forward(self, x):
+        for layer in self._layers:
+            x = layer(x)
+
+        return torch.relu(x)
 
 
 class Encoder(nn.Module):

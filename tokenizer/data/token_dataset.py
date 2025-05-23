f360de732ee617a3dd36cd756ccfd94069184971
fabP258 <fabio894@gmail.com>
2025-05-23 15:01:32 +0200

    Adapt dataset for proper autoregressive transformer training

--- tokenizer/data/token_dataset.py
+++ tokenizer/data/token_dataset.py
@@ -6,30 +6,41 @@ import torch
 from torch.utils.data import Dataset
 
 
+def increment_slice(s: slice) -> slice:
+    return slice(s.start + 1, s.stop + 1, s.step)
+
+
 class ImageTokenDataset(Dataset):
 
-    def __init__(self, token_root_path: Union[str, Path]):
+    def __init__(
+        self, token_root_path: Union[str, Path], n_cond_frames: int, H: int, W: int
+    ):
         super().__init__()
-        self.token_files = list(Path(token_root_path).rglob("*.npy"))
+
+        self.token_files = []
+        self.n_cond_frames = n_cond_frames
+        self.H = H
+        self.W = W
 
         num_token_sequences = []
 
-        for tf in self.token_files:
+        for tf in Path(token_root_path).rglob("*.npy"):
             token_tensor = np.load(tf, mmap_mode="r")
+            if len(token_tensor.shape) != 3:
+                continue
+            F, H, W = token_tensor.shape
+            if (H != self.H) or (W != self.W) or (F <= n_cond_frames):
+                continue
+
+            self.token_files.append(tf)
             # calculate the number of token sequences that can be sampled from the tensor
-            # reduced by 1 to account for the shifted target sequence
-            num_token_sequences.append(
-                token_tensor.size - (token_tensor.shape[-2] * token_tensor.shape[-1])
-            )
+            # there are F-C frames to be predicted
+            # for each predicted frame there can H*W token sequences be sampled
+            num_token_sequences.append((F - n_cond_frames) * H * W)
 
         self.cumulative_lengths = np.cumsum(
             num_token_sequences
         )  # used to map index to token_file
-
-        # TODO: derive these from loaded tensors
-        self.H = 18
-        self.W = 32
-        self.sequence_length = self.H * self.W
 
     def __len__(self):
         return self.cumulative_lengths[-1]
@@ -44,38 +55,28 @@ class ImageTokenDataset(Dataset):
                 return tnsr_idx, local_idx
         raise IndexError("Global index out of range")
 
-    def _calc_position_indices(
-        self, start_index: int, grid: bool = False
-    ) -> torch.Tensor:
-        """
-        Calculates 1D position indices indicating the location of the image patch.
-        If `grid==True` the position are rearranged on a 2D grid in row-major order.
-        """
-
-        position_indices = torch.tensor(
-            [
-                (i + start_index) % self.sequence_length
-                for i in range(self.sequence_length)
-            ],
+    def _calc_sequence_index_slice(self, local_idx: int) -> slice:
+        n_predictable_sequences = self.H * self.W
+        frame_idx = local_idx // n_predictable_sequences
+        start_idx = frame_idx * n_predictable_sequences
+        end_idx = (
+            start_idx
+            + n_predictable_sequences * self.n_cond_frames
+            + local_idx % n_predictable_sequences
         )
-        if not grid:
-            return position_indices
-        return position_indices.view(self.H, self.W)
+        return slice(start_idx, end_idx)
 
     def __getitem__(self, index):
-        tnsr_idx, seq_start_idx = self._map_global_index_to_tensor_index(index)
-        token_tensor = np.load(self.token_files[tnsr_idx], mmap_mode="r").reshape(-1)
-        input_sequence = token_tensor[
-            seq_start_idx : seq_start_idx + self.sequence_length
-        ]
-        seq_start_idx += 1
-        target_sequence = token_tensor[
-            seq_start_idx : seq_start_idx + self.sequence_length
-        ]
-        position_indices = self._calc_position_indices(index)
+        tnsr_idx, local_seq_idx = self._map_global_index_to_tensor_index(index)
+        token_sequence_slice = self._calc_sequence_index_slice(local_seq_idx)
+
+        flat_token_tensor = np.load(self.token_files[tnsr_idx], mmap_mode="r").reshape(
+            -1
+        )
+        input_sequence = flat_token_tensor[token_sequence_slice]
+        target_sequence = flat_token_tensor[increment_slice(token_sequence_slice)]
 
         return (
             torch.tensor(input_sequence),
             torch.tensor(target_sequence),
-            position_indices,
         )
